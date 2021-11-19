7bb735e7369a7df218e26382c41343ca3bd8405a
niuhuan <niuhuancn@outlook.com>
2021-11-19 17:07:37 +0800

    update windows application info

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -30,7 +30,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(&run_loop, project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"pikapi", origin, size)) {
+  if (!window.CreateAndShow(L"pikapika", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
