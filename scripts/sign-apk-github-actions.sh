a3cdc6f99e58962ea3bab7c5a396ef53abf07633
niuhuan <niuhuancn@outlook.com>
2022-03-31 22:35:24 +0800

    Set android home

--- scripts/sign-apk-github-actions.sh
+++ scripts/sign-apk-github-actions.sh
@@ -3,4 +3,4 @@ cd "$( cd "$( dirname "$0"  )" && pwd  )/.."
 
 echo $KEY_FILE_BASE64 > key.jks.base64
 base64 -d key.jks.base64 > key.jks
-echo $KEY_PASSWORD | apksigner sign --ks key.jks build/app/outputs/flutter-apk/app-release.apk
+echo $KEY_PASSWORD | ANDROID_HOME/build-tools/30.0.2/apksigner sign --ks key.jks build/app/outputs/flutter-apk/app-release.apk
