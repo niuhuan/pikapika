ca712fbc892e7acaf9dd51fecaad20d7a7a9d94c
niuhuan <niuhuancn@outlook.com>
2024-08-19 10:46:41 +0800

    :construction_worker: android ndk update

--- scripts/build-apk-x86.sh
+++ scripts/build-apk-x86.sh
@@ -4,6 +4,6 @@ cd "$( cd "$( dirname "$0"  )" && pwd  )/.."
 
 cd go/mobile
 gomobile init
-gomobile bind -androidapi 19 -target=android/386 -o lib/Mobile.aar ./
+gomobile bind -androidapi 21 -target=android/386 -o lib/Mobile.aar ./
 cd ../..
 flutter build apk --target-platform android-x86
